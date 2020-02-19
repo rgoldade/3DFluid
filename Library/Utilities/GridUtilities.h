aae8ac9f161f396ad2df7bff814a3781a70674cf
Ryan Goldade <rgoldade@uwaterloo.ca>
2020-02-18 22:54:54 -0500

    Fixed indentation

--- Library/Utilities/GridUtilities.h
+++ Library/Utilities/GridUtilities.h
@@ -6,260 +6,262 @@
 
 namespace FluidSim3D::Utilities
 {
-	enum class Axis { XAXIS, YAXIS, ZAXIS };
 
-	inline Vec3i cellToCell(const Vec3i& cell, int axis, int direction)
+enum class Axis { XAXIS, YAXIS, ZAXIS };
+
+inline Vec3i cellToCell(const Vec3i& cell, int axis, int direction)
+{
+	Vec3i adjacentCell(cell);
+
+	if (direction == 0)
+		--adjacentCell[axis];
+	else
 	{
-		Vec3i adjacentCell(cell);
+		assert(direction == 1);
+		++adjacentCell[axis];
+	}
 
-		if (direction == 0)
-			--adjacentCell[axis];
-		else
+	return adjacentCell;
+}
+
+inline Vec3i cellToFace(const Vec3i& cell, int axis, int direction)
+{
+	Vec3i face(cell);
+
+	if (direction == 1)
+		++face[axis];
+	else assert(direction == 0);
+
+	return face;
+}
+
+inline Vec3i cellToEdge(const Vec3i& cell, int edgeAxis, int edgeIndex)
+{
+	assert(edgeAxis >= 0 && edgeAxis < 3);
+	assert(edgeIndex >= 0 && edgeIndex < 4);
+
+	Vec3i edge(cell);
+
+	for (int axisOffset : {0, 1})
+	{
+		if (edgeIndex & (1 << axisOffset))
 		{
-			assert(direction == 1);
-			++adjacentCell[axis];
+			int localAxis = (edgeAxis + 1 + axisOffset) % 3;
+			++edge[localAxis];
 		}
-
-		return adjacentCell;
 	}
 
-	inline Vec3i cellToFace(const Vec3i& cell, int axis, int direction)
+	return edge;
+}
+
+inline Vec3i cellToNode(const Vec3i& cell, int nodeIndex)
+{
+	assert(nodeIndex >= 0 && nodeIndex < 8);
+
+	Vec3i node(cell);
+	for (int axis : {0, 1, 2})
 	{
-		Vec3i face(cell);
-
-		if (direction == 1)
-			++face[axis];
-		else assert(direction == 0);
-
-		return face;
-	}
-
-	inline Vec3i cellToEdge(const Vec3i& cell, int edgeAxis, int edgeIndex)
-	{
-		assert(edgeAxis >= 0 && edgeAxis < 3);
-		assert(edgeIndex >= 0 && edgeIndex < 4);
-
-		Vec3i edge(cell);
-
-		for (int axisOffset : {0, 1})
-		{
-			if (edgeIndex & (1 << axisOffset))
-			{
-				int localAxis = (edgeAxis + 1 + axisOffset) % 3;
-				++edge[localAxis];
-			}
-		}
-
-		return edge;
-	}
-
-	inline Vec3i cellToNode(const Vec3i& cell, int nodeIndex)
-	{
-		assert(nodeIndex >= 0 && nodeIndex < 8);
-
-		Vec3i node(cell);
-		for (int axis : {0, 1, 2})
-		{
-			if (nodeIndex & (1 << axis))
-				++node[axis];
-		}
-
-		return node;
-	}
-
-	inline Vec3i faceToCell(const Vec3i& face, int axis, int direction)
-	{
-		Vec3i cell(face);
-		if (direction == 0)
-			--cell[axis];
-		else
-			assert(direction == 1);
-
-		return cell;
-	}
-
-	inline Vec3i faceToEdge(const Vec3i& face, int faceAxis, int edgeAxis, int direction)
-	{
-		assert(faceAxis >= 0 && faceAxis < 3 && edgeAxis >= 0 && edgeAxis < 3);
-		assert(faceAxis != edgeAxis);
-
-		Vec3i edge(face);
-		if (direction == 1)
-		{
-			int offsetAxis = 3 - faceAxis - edgeAxis;
-			++edge[offsetAxis];
-		}
-		else
-			assert(direction == 0);
-
-		return edge;
-	}
-
-	inline Vec3i faceToNode(const Vec3i& face, int faceAxis, int nodeIndex)
-	{
-		assert(faceAxis >= 0 && faceAxis < 3);
-		assert(nodeIndex >= 0 && nodeIndex < 4);
-
-		Vec3i node(face);
-		for (int axisOffset : {0, 1})
-		{
-			if (nodeIndex & (1 << axisOffset))
-			{
-				int localAxis = (faceAxis + 1 + axisOffset) % 3;
-				++node[localAxis];
-			}
-		}
-
-		return node;
-	}
-
-	inline Vec3i faceToNodeCCW(const Vec3i& face, int faceAxis, int nodeIndex)
-	{
-		const Vec3i faceToNodeOffsets[3][4] = { { Vec3i(0,0,0), Vec3i(0,1,0), Vec3i(0,1,1), Vec3i(0,0,1) },
-												{ Vec3i(0,0,0), Vec3i(0,0,1), Vec3i(1,0,1), Vec3i(1,0,0) },
-												{ Vec3i(0,0,0), Vec3i(1,0,0), Vec3i(1,1,0), Vec3i(0,1,0) } };
-
-		assert(faceAxis >= 0 && faceAxis < 3);
-		assert(nodeIndex >= 0 && nodeIndex < 4);
-
-		Vec3i node(face);
-		node += faceToNodeOffsets[faceAxis][nodeIndex];
-
-		return node;
-	}
-
-	inline Vec3i edgeToFace(const Vec3i& edge, int edgeAxis, int faceAxis, int direction)
-	{
-		assert(faceAxis >= 0 && faceAxis < 3 && edgeAxis >= 0 && edgeAxis < 3);
-		assert(faceAxis != edgeAxis);
-
-		Vec3i face(edge);
-		if (direction == 0)
-		{
-			int offsetAxis = 3 - faceAxis - edgeAxis;
-			--face[offsetAxis];
-		}
-		else
-			assert(direction == 1);
-
-		return face;
-	}
-
-	inline Vec3i edgeToCell(const Vec3i& edge, int edgeAxis, int cellIndex)
-	{
-		assert(edgeAxis >= 0 && edgeAxis < 3);
-		assert(cellIndex >= 0 && cellIndex < 4);
-
-		Vec3i cell(edge);
-		for (int axisOffset : {0, 1})
-		{
-			if (!(cellIndex & (1 << axisOffset)))
-			{
-				int localAxis = (edgeAxis + 1 + axisOffset) % 3;
-				--cell[localAxis];
-			}
-		}
-
-		return cell;
-	}
-
-	inline Vec3i edgeToCellCCW(const Vec3i& edge, int edgeAxis, int cellIndex)
-	{
-		const Vec3i edgeToCellOffsets[3][4] = { { Vec3i( 0,-1,-1), Vec3i( 0, 0,-1), Vec3i( 0, 0, 0), Vec3i( 0,-1, 0) },
-												{ Vec3i(-1, 0,-1), Vec3i(-1, 0, 0), Vec3i( 0, 0, 0), Vec3i( 0, 0,-1) },
-												{ Vec3i(-1,-1, 0), Vec3i( 0,-1, 0), Vec3i( 0, 0, 0), Vec3i(-1, 0, 0) } };
-
-		assert(edgeAxis >= 0 && edgeAxis < 3);
-		assert(cellIndex >= 0 && cellIndex < 4);
-
-		Vec3i cell(edge);
-		cell += edgeToCellOffsets[edgeAxis][cellIndex];
-
-		return cell;
-	}
-
-	inline Vec3i edgeToNode(const Vec3i& edge, int axis, int direction)
-	{
-		Vec3i node(edge);
-		if (direction == 1)
+		if (nodeIndex & (1 << axis))
 			++node[axis];
-		else assert(direction == 0);
-
-		return node;
 	}
 
-	inline Vec3i nodeToFace(const Vec3i& node, int faceAxis, int faceIndex)
-	{
-		assert(faceAxis >= 0 && faceAxis < 3);
-		assert(faceIndex >= 0 && faceIndex < 4);
+	return node;
+}
 
-		Vec3i face(node);
-		for (int axisOffset : {0, 1})
+inline Vec3i faceToCell(const Vec3i& face, int axis, int direction)
+{
+	Vec3i cell(face);
+	if (direction == 0)
+		--cell[axis];
+	else
+		assert(direction == 1);
+
+	return cell;
+}
+
+inline Vec3i faceToEdge(const Vec3i& face, int faceAxis, int edgeAxis, int direction)
+{
+	assert(faceAxis >= 0 && faceAxis < 3 && edgeAxis >= 0 && edgeAxis < 3);
+	assert(faceAxis != edgeAxis);
+
+	Vec3i edge(face);
+	if (direction == 1)
+	{
+		int offsetAxis = 3 - faceAxis - edgeAxis;
+		++edge[offsetAxis];
+	}
+	else
+		assert(direction == 0);
+
+	return edge;
+}
+
+inline Vec3i faceToNode(const Vec3i& face, int faceAxis, int nodeIndex)
+{
+	assert(faceAxis >= 0 && faceAxis < 3);
+	assert(nodeIndex >= 0 && nodeIndex < 4);
+
+	Vec3i node(face);
+	for (int axisOffset : {0, 1})
+	{
+		if (nodeIndex & (1 << axisOffset))
 		{
-			if (!(faceIndex & (1 << axisOffset)))
-			{
-				int localAxis = (faceAxis + 1 + axisOffset) % 3;
-				--face[localAxis];
-			}
+			int localAxis = (faceAxis + 1 + axisOffset) % 3;
+			++node[localAxis];
 		}
-
-		return face;
 	}
 
-	inline Vec3i nodeToCell(const Vec3i& node, int cellIndex)
-	{
-		assert(cellIndex >= 0 && cellIndex < 8);
+	return node;
+}
 
-		Vec3i cell(node);
-		for (int axis : {0, 1, 2})
+inline Vec3i faceToNodeCCW(const Vec3i& face, int faceAxis, int nodeIndex)
+{
+	const Vec3i faceToNodeOffsets[3][4] = { { Vec3i(0,0,0), Vec3i(0,1,0), Vec3i(0,1,1), Vec3i(0,0,1) },
+											{ Vec3i(0,0,0), Vec3i(0,0,1), Vec3i(1,0,1), Vec3i(1,0,0) },
+											{ Vec3i(0,0,0), Vec3i(1,0,0), Vec3i(1,1,0), Vec3i(0,1,0) } };
+
+	assert(faceAxis >= 0 && faceAxis < 3);
+	assert(nodeIndex >= 0 && nodeIndex < 4);
+
+	Vec3i node(face);
+	node += faceToNodeOffsets[faceAxis][nodeIndex];
+
+	return node;
+}
+
+inline Vec3i edgeToFace(const Vec3i& edge, int edgeAxis, int faceAxis, int direction)
+{
+	assert(faceAxis >= 0 && faceAxis < 3 && edgeAxis >= 0 && edgeAxis < 3);
+	assert(faceAxis != edgeAxis);
+
+	Vec3i face(edge);
+	if (direction == 0)
+	{
+		int offsetAxis = 3 - faceAxis - edgeAxis;
+		--face[offsetAxis];
+	}
+	else
+		assert(direction == 1);
+
+	return face;
+}
+
+inline Vec3i edgeToCell(const Vec3i& edge, int edgeAxis, int cellIndex)
+{
+	assert(edgeAxis >= 0 && edgeAxis < 3);
+	assert(cellIndex >= 0 && cellIndex < 4);
+
+	Vec3i cell(edge);
+	for (int axisOffset : {0, 1})
+	{
+		if (!(cellIndex & (1 << axisOffset)))
 		{
-			if (!(cellIndex & (1 << axis)))
-				--cell[axis];
+			int localAxis = (edgeAxis + 1 + axisOffset) % 3;
+			--cell[localAxis];
 		}
-
-		return cell;
 	}
 
-	const Vec3f colours[] = { Vec3f(1,0,0), Vec3f(0,1,0), Vec3f(0,0,1), Vec3f(1,1,0), Vec3f(1,0,1), Vec3f(0,1,1) };
+	return cell;
+}
 
-	template<typename Real>
-	Real lengthFraction(Real phi0, Real phi1)
+inline Vec3i edgeToCellCCW(const Vec3i& edge, int edgeAxis, int cellIndex)
+{
+	const Vec3i edgeToCellOffsets[3][4] = { { Vec3i( 0,-1,-1), Vec3i( 0, 0,-1), Vec3i( 0, 0, 0), Vec3i( 0,-1, 0) },
+											{ Vec3i(-1, 0,-1), Vec3i(-1, 0, 0), Vec3i( 0, 0, 0), Vec3i( 0, 0,-1) },
+											{ Vec3i(-1,-1, 0), Vec3i( 0,-1, 0), Vec3i( 0, 0, 0), Vec3i(-1, 0, 0) } };
+
+	assert(edgeAxis >= 0 && edgeAxis < 3);
+	assert(cellIndex >= 0 && cellIndex < 4);
+
+	Vec3i cell(edge);
+	cell += edgeToCellOffsets[edgeAxis][cellIndex];
+
+	return cell;
+}
+
+inline Vec3i edgeToNode(const Vec3i& edge, int axis, int direction)
+{
+	Vec3i node(edge);
+	if (direction == 1)
+		++node[axis];
+	else assert(direction == 0);
+
+	return node;
+}
+
+inline Vec3i nodeToFace(const Vec3i& node, int faceAxis, int faceIndex)
+{
+	assert(faceAxis >= 0 && faceAxis < 3);
+	assert(faceIndex >= 0 && faceIndex < 4);
+
+	Vec3i face(node);
+	for (int axisOffset : {0, 1})
 	{
-		Real theta = 0.;
-
-		if (phi0 < 0)
+		if (!(faceIndex & (1 << axisOffset)))
 		{
-			if (phi1 < 0)
-				theta = 1;
-			else if (phi1 >= 0)
-				theta = phi0 / (phi0 - phi1);
+			int localAxis = (faceAxis + 1 + axisOffset) % 3;
+			--face[localAxis];
 		}
-		else if (phi1 < 0)
-			theta = phi1 / (phi1 - phi0);
-
-		return theta;
 	}
 
-	// Execute function "f" over range [start, end)
-	template<typename T, typename Function>
-	void forEachVoxelRange(const Vec<3, T>& start, const Vec<3, T>& end, const Function& f)
+	return face;
+}
+
+inline Vec3i nodeToCell(const Vec3i& node, int cellIndex)
+{
+	assert(cellIndex >= 0 && cellIndex < 8);
+
+	Vec3i cell(node);
+	for (int axis : {0, 1, 2})
 	{
-		Vec<3, T> cell;
-		for (cell[0] = start[0]; cell[0] < end[0]; ++cell[0])
-			for (cell[1] = start[1]; cell[1] < end[1]; ++cell[1])
-				for (cell[2] = start[2]; cell[2] < end[2]; ++cell[2])
-					f(cell);
+		if (!(cellIndex & (1 << axis)))
+			--cell[axis];
 	}
 
-	template<typename T, typename Function>
-	void forEachVoxelRangeReverse(const Vec<3, T>& start, const Vec<3, T>& end, const Function& f)
+	return cell;
+}
+
+const Vec3f colours[] = { Vec3f(1,0,0), Vec3f(0,1,0), Vec3f(0,0,1), Vec3f(1,1,0), Vec3f(1,0,1), Vec3f(0,1,1) };
+
+template<typename Real>
+Real lengthFraction(Real phi0, Real phi1)
+{
+	Real theta = 0.;
+
+	if (phi0 < 0)
 	{
-		Vec<3, T> cell;
-		for (cell[0] = end[0] - 1; cell[0] >= start[0]; --cell[0])
-			for (cell[1] = end[1] - 1; cell[1] >= start[1]; --cell[1])
-				for (cell[2] = end[2] - 1; cell[2] >= start[2]; --cell[2])
-					f(cell);
+		if (phi1 < 0)
+			theta = 1;
+		else if (phi1 >= 0)
+			theta = phi0 / (phi0 - phi1);
 	}
+	else if (phi1 < 0)
+		theta = phi1 / (phi1 - phi0);
+
+	return theta;
+}
+
+// Execute function "f" over range [start, end)
+template<typename T, typename Function>
+void forEachVoxelRange(const Vec<3, T>& start, const Vec<3, T>& end, const Function& f)
+{
+	Vec<3, T> cell;
+	for (cell[0] = start[0]; cell[0] < end[0]; ++cell[0])
+		for (cell[1] = start[1]; cell[1] < end[1]; ++cell[1])
+			for (cell[2] = start[2]; cell[2] < end[2]; ++cell[2])
+				f(cell);
+}
+
+template<typename T, typename Function>
+void forEachVoxelRangeReverse(const Vec<3, T>& start, const Vec<3, T>& end, const Function& f)
+{
+	Vec<3, T> cell;
+	for (cell[0] = end[0] - 1; cell[0] >= start[0]; --cell[0])
+		for (cell[1] = end[1] - 1; cell[1] >= start[1]; --cell[1])
+			for (cell[2] = end[2] - 1; cell[2] >= start[2]; --cell[2])
+				f(cell);
+}
+
 }
 
 #endif
